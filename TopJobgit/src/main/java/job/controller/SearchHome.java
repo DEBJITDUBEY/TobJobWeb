ab670cc8877aa91b3a335fb58c1dc60b5bc66dfe
DEBJITDUBEY <debjitbcet@gmail.com>
2019-08-30 09:53:21 +0530

    Commit

---
+++ TopJobgit/src/main/java/job/controller/SearchHome.java
@@ -0,0 +1,30 @@
+package job.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SearchHome
+ */
+public class SearchHome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchHome() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
